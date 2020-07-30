e3e2ca15391f95180e9a46f0e8ea46150204a130
saraencci <franksaraencci@gmail.com>
2020-07-30 23:59:55 +0300

    added rertrieval url array list

--- readNews.php
+++ readNews.php
@@ -4,39 +4,38 @@ require_once('includes/config.php');
 require_once('includes/private.php');
 require_once('includes/database.class.php');
 $db= new database($pdo);
-$json = file_get_contents($private_url);
+$no=sizeof($private_url);
+foreach($private_url as $url)
+{
+  $json = file_get_contents($url);
 $json = json_decode($json);
 
 foreach ($json->articles as $article) {
   if($db->isSiteAvailible($article->url)){
+    try{
     $html = file_get_html($article->url);
+      }
+      catch(Exception $r){
+
+      }
     $str="";
     foreach($html->find('p') as $element)
          $str=$str.$element;
-        
 
+         if($str!=""){
+         try{
+          $db->add_articles_to_database($article->title ,$article->author ,$article->description 
+          ,$article->url,$article->urlToImage,$str);
 
-        
-    $stat=(!$db->check_if_article_exists_in_tabe($article->url));
-    if($stat){
-
-      $db->add_articles_to_database($article->title ,$article->author ,$article->description 
-      ,$article->url,$article->urlToImage,$str);
-
-       // $stat=(!$db->check_if_article_exists_in_tabe($article->url));
-        echo "inside loop";
-        //article doesn't exist so inserting
-        // $db->add_articles_to_database($article->title ,$article->author ,$article->description 
-        // ,$article->url,$article->ururlToImagel,$str);
-
-
-    }
+         }
+        catch(PDOException $e){
+          echo "already present in db";
+        }
+      }
     echo "---------------------------------------------------------------------------------------<br>";
     }
-   
-    
- }
-
+   }
+  }
 
 
 ?>
